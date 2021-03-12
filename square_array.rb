c530a79877d6e26ffcb90bef0ef3bbaaece8d599
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-12 22:02:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -10,3 +10,19 @@ def square_array(array)
   end
   new_numbers
 end
+
+
+
+
+
+
+# This is wrong because ln 21 the character * isnt being interpreted as multiplication. new_numbers is being called AFTER it has been stored
+
+# def square_array(array)
+#
+#   new_numbers = []
+#   array.each do |number|
+#      "#{number} * #{number}"
+#   end
+#   new_numbers << array
+# end
